ec45b2b5228d3c60b31946c67f28667a409eef62
Edward Lee <edward.jay.lee@gmail.com>
2019-11-12 10:19:44 +1100

    ejle additions

--- layouts/community/ergodox/ejle/keymap.c
+++ layouts/community/ergodox/ejle/keymap.c
@@ -282,22 +282,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 6: Full linux layer
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |   -  |           |  +   |   6  |   7  |  8   |    9 |   0  |  bksp  |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |   -  |           |  +   |   6  |   7  |  8   |    9 |   0  |  del   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |tab/MDIA|   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |`       |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |esc/ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |'  /ctl |
+ * |tab     |   A  |   S  |   D  |F/MDIA|   G  |------|           |------|   H  |J/MDIA|   K  |   L  |   ;  |'       |
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
  * | LS/PO  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RS/PC  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  ctl |      |      |  gui |sp/alt|                                       |sp/alt| left | down |   up | right|
+ *   |  ctl |      |      |  gui | alt  |                                       |   alt| left | down |   up | right|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
  *                                        | ~L8  |   L7 |       | L0   |  ~L8   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | pgup |        |      |
  *                                 |bkspc | esc/ |------|       |------| enter/ |space | (tapdance: ?)
- *                                 |      | MDIA |  end |       | pgdn |  MDIA  |      |
+ *                                 |      | ctrl |  end |       | pgdn |  ctrl  |      |
  *                                 `--------------------'       `----------------------'
  */
 
@@ -305,69 +305,70 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [FULL_BASE] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,        KC_1,  KC_2,  KC_3,    KC_4,     KC_5, KC_MINS,
-        LT(FULL_MDIA, KC_TAB),         KC_Q,  KC_W,  KC_E,    KC_R,    KC_T,  KC_LCBR,
-        CTL_T(KC_ESC), KC_A,  KC_S,  KC_D,    KC_F,         KC_G,
-        KC_LSPO,       KC_Z,  KC_X,  KC_C,    KC_V,         KC_B,  KC_LBRC,
-        KC_LCTL,       KC_NO, KC_NO, KC_LGUI, LALT_T(KC_SPC),
-                                                         TG(FULL_MDIA),       TO(FULL_MAC),
-                                                                              KC_HOME,
-                                              KC_BSPC, LT(FULL_MDIA, KC_ESC), KC_END,
+        KC_GRV,  KC_1,  KC_2,  KC_3,    KC_4,                KC_5, KC_MINS,
+        KC_GRV,  KC_Q,  KC_W,  KC_E,    KC_R,                KC_T, KC_LCBR,
+        KC_TAB,  KC_A,  KC_S,  KC_D,    LT(FULL_MDIA, KC_F), KC_G,
+        KC_LSPO, KC_Z,  KC_X,  KC_C,    KC_V,                KC_B, KC_LBRC,
+        KC_LCTL, KC_NO, KC_NO, KC_LGUI, KC_LALT,
+                                                         TG(FULL_MDIA), TO(FULL_MAC),
+                                                                        KC_HOME,
+                                               KC_BSPC, LCTL_T(KC_ESC), KC_END,
         // right hand
-             KC_EQL,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-             KC_RCBR, KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
-                      KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(KC_QUOT),
-             KC_RBRC, KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-                             RALT_T(KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+             KC_EQL,  KC_6,  KC_7,                KC_8,    KC_9,    KC_0,    KC_DEL,
+             KC_RCBR, KC_Y,  KC_U,                KC_I,    KC_O,    KC_P,    KC_BSLS,
+                      KC_H,  LT(FULL_MDIA, KC_J), KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+             KC_RBRC, KC_N,  KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+                             KC_RALT,             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
 
              TO(BASE), TG(FULL_MDIA),
              KC_PGUP,
-             KC_DOWN,    LT(FULL_MDIA, KC_ENT), KC_SPC
+             KC_PGDOWN,  RCTL_T(KC_ENT), KC_SPC
     ),
-/* Keymap 6: Full mac layer
+/* Keymap 6: Full mac layer -> flip alt and gui
+ *
  *
  * ,--------------------------------------------------.           ,--------------------------------------------------.
- * |   `    |   1  |   2  |   3  |   4  |   5  |   -  |           |  +   |   6  |   7  |  8   |    9 |   0  |  bksp  |
+ * |   `    |   1  |   2  |   3  |   4  |   5  |   -  |           |  +   |   6  |   7  |  8   |    9 |   0  |  del   |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |tab/MDIA|   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * |`       |   Q  |   W  |   E  |   R  |   T  |  {   |           |  }   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |esc/ctrl|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |'  /ctl |
+ * |tab     |   A  |   S  |   D  |F/MDIA|   G  |------|           |------|   H  |J/MDIA|   K  |   L  |   ;  |'       |
  * |--------+------+------+------+------+------|  [   |           |   ]  |------+------+------+------+------+--------|
  * | LS/PO  |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RS/PC  |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |  ctl |      |      |  gui |sp/alt|                                       |sp/alt| left | down |   up | right|
+ *   |  ctl |      |      |  alt | gui  |                                       |   gui| left | down |   up | right|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,---------------.
- *                                        | ~L8  |   L6 |       | L0   |  ~L8   |
+ *                                        | ~L8  |   L7 |       | L0   |  ~L8   |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | pgup |        |      |
  *                                 |bkspc | esc/ |------|       |------| enter/ |space | (tapdance: ?)
- *                                 |      | MDIA |  end |       | pgdn |  MDIA  |      |
+ *                                 |      | ctrl |  end |       | pgdn |  ctrl  |      |
  *                                 `--------------------'       `----------------------'
  */
 
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
 // Otherwise, it needs KC_*
-[FULL_BASE] = LAYOUT_ergodox(  // layer 0 : default
+[FULL_MAC] = LAYOUT_ergodox(  // layer 0 : default
         // left hand
-        KC_GRV,        KC_1,  KC_2,  KC_3,    KC_4,     KC_5, KC_MINS,
-LT(FULL_MDIA, KC_TAB), KC_Q,  KC_W,  KC_E,    KC_R,    KC_T,  KC_LCBR,
-        CTL_T(KC_ESC), KC_A,  KC_S,  KC_D,    KC_F,         KC_G,
-        KC_LSPO,       KC_Z,  KC_X,  KC_C,    KC_V,         KC_B,  KC_LBRC,
-        KC_LCTL,       KC_NO, KC_NO, KC_LALT, LGUI_T(KC_SPC),
-                                                         TG(FULL_MDIA),       TO(FULL_BASE),
-                                                                              KC_HOME,
-                                              KC_BSPC, LT(FULL_MDIA, KC_ESC), KC_END,
+        KC_GRV,  KC_1,  KC_2,  KC_3,    KC_4,                KC_5, KC_MINS,
+        KC_GRV,  KC_Q,  KC_W,  KC_E,    KC_R,                KC_T, KC_LCBR,
+        KC_TAB,  KC_A,  KC_S,  KC_D,    LT(FULL_MDIA, KC_F), KC_G,
+        KC_LSPO, KC_Z,  KC_X,  KC_C,    KC_V,                KC_B, KC_LBRC,
+        KC_LCTL, KC_NO, KC_NO, KC_LALT, KC_LGUI,
+                                                         TG(FULL_MDIA), TO(FULL_BASE),
+                                                                        KC_HOME,
+                                               KC_BSPC, LCTL_T(KC_ESC), KC_END,
         // right hand
-             KC_EQL,  KC_6,  KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
-             KC_RCBR, KC_Y,  KC_U,    KC_I,    KC_O,    KC_P,    KC_NO,
-                      KC_H,  KC_J,    KC_K,    KC_L,    KC_SCLN, CTL_T(KC_QUOT),
-             KC_RBRC, KC_N,  KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
-                             RGUI_T(KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
+             KC_EQL,  KC_6,  KC_7,                KC_8,    KC_9,    KC_0,    KC_DEL,
+             KC_RCBR, KC_Y,  KC_U,                KC_I,    KC_O,    KC_P,    KC_BSLS,
+                      KC_H,  LT(FULL_MDIA, KC_J), KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+             KC_RBRC, KC_N,  KC_M,                KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC,
+                             KC_RGUI,             KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,
 
              TO(BASE), TG(FULL_MDIA),
              KC_PGUP,
-             KC_DOWN,    LT(FULL_MDIA, KC_ENT), KC_SPC
+             KC_PGDOWN,  RCTL_T(KC_ENT), KC_SPC
     ),
 /* Keymap 8: Full Media and mouse keys
  *
